e0775c830da0763df8c3ff4ce86ac5896dc0f35b
Israel Antezana <israelantezana@gmail.com>
2023-09-26 09:23:58 -0400

    First test

---
+++ src/BowlingGame.test.js
@@ -0,0 +1,14 @@
+const Game = require ("./Game.js");
+let g;
+beforeEach(() => {
+  g = new Game();
+})
+
+describe("Bowling Game Test", () => {
+  it("deberia devolver 0 porque el jugador no derribo ningun pin en los 10 turnos", () => {
+    for(let i=0; i < 20; i ++){
+      g.roll(0);
+    }
+    expect(g.score()).toEqual(0);
+  });
+});
---
+++ src/Game.js
@@ -0,0 +1,14 @@
+class Game{
+  constructor(){
+    this.rolls = [];
+  }
+  roll(pins){
+    this.rolls.push(pins);
+  }
+  score(){
+    return 0;
+  }
+  
+}
+
+module.exports = Game;
