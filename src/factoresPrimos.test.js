d479dfbe034b0f59c5886a1bb43d02417f248a24
Israel Antezana <israelantezana@gmail.com>
2023-09-26 09:41:39 -0400

    deberia retornar una lista para [2]

---
+++ src/factoresPrimos.test.js
@@ -0,0 +1,24 @@
+
+describe("factores primos", () => {
+  it("deberia retornar una lista vacia para 1", () => {
+    const primosfac = new primos();
+    expect(primosfac.factorprimo(1)).toEqual([]);
+  });
+  it("deberia retornar una lista para [2]", () => {
+    const primosfac = new primos();
+    expect(primosfac.factorprimo(2)).toEqual([2]);
+  });
+  
+});
+
+
+class primos{
+  factorprimo(n){
+    let resulprimos = [];
+    if (n>1)
+    {
+      resulprimos.push(2);
+    }
+    return resulprimos;
+  }
+}
